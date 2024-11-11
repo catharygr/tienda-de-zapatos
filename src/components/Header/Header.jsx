99397ccda0edf75ab75da51e6525ca0bdc2630eb
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-11 13:39:08 +0000

    Trabajando en el componente SuperHeader.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
 import Link from "next/link";
 import styles from "./Header.module.css";
+import Logo from "../Logo";
 
 export default function Header() {
   return (
     <header>
-      <h1>My App</h1>
+      <div className={styles.mainHeader}>
+        <Logo />
+        <nav className={styles.a}>
+          <Link href="/sale">Sale</Link>
+          <Link href="/new">New&nbsp;Releases</Link>
+          <Link href="/men">Men</Link>
+          <Link href="/women">Women</Link>
+          <Link href="/kids">Kids</Link>
+          <Link href="/collections">Collections</Link>
+        </nav>
+      </div>
     </header>
   );
 }
