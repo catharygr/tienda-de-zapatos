e3013144535b0de1a5f1eff951ec60d9407ba734
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-11 17:48:58 +0000

    Trabajando en el componente Icon.jsx

--- src/components/SuperHeader/SuperHeader.jsx
+++ src/components/SuperHeader/SuperHeader.jsx
@@ -2,6 +2,7 @@ import styles from "./SuperHeader.module.css";
 
 import SearchInput from "../SearchInput";
 import Icon from "../Icon";
+import Link from "next/link";
 
 export default function SuperHeader() {
   return (
@@ -11,6 +12,12 @@ export default function SuperHeader() {
         Free shipping on domestic orders over $75!
       </span>
       <SearchInput />
+      <Link
+        className={styles.helpLink}
+        href="/help"
+      >
+        Help
+      </Link>
       <button className={styles.button}>
         <Icon id="shopping-bag" />
       </button>
