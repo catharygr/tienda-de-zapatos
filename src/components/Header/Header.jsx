1e4837f47d1e36392a92b251d9f831a6ab34f0e7
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-12 12:47:34 +0000

    Trabajando en el css del  componente SuperHeader.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,19 +7,17 @@ export default function Header() {
   // Nuestro sitio presenta dos encabezados visuales, pero deberían
   // agruparse semánticamente como un solo encabezado.
   return (
-    <header>
-      <div className={styles.mainHeader}>
-        <SuperHeader />
-        <Logo />
-        <nav className={styles.enlaces}>
-          <Link href="/sale">Sale</Link>
-          <Link href="/new">New&nbsp;Releases</Link>
-          <Link href="/men">Men</Link>
-          <Link href="/women">Women</Link>
-          <Link href="/kids">Kids</Link>
-          <Link href="/collections">Collections</Link>
-        </nav>
-      </div>
+    <header className={styles.mainHeader}>
+      <SuperHeader />
+      <Logo />
+      <nav className={styles.enlaces}>
+        <Link href="/sale">Sale</Link>
+        <Link href="/new">New&nbsp;Releases</Link>
+        <Link href="/men">Men</Link>
+        <Link href="/women">Women</Link>
+        <Link href="/kids">Kids</Link>
+        <Link href="/collections">Collections</Link>
+      </nav>
     </header>
   );
 }
--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -4,22 +4,17 @@ import styles from "./SearchInput.module.css";
 
 export default function SearchInput({ label, ...delegated }) {
   return (
-    <form className={styles.wrapperForm}>
-      <label
-        htmlFor="input"
-        className={styles.label}
-      >
-        <VisuallyHidden>Search</VisuallyHidden>
-        <input
-          className={styles.input}
-          {...delegated}
-          placeholder="Search"
-        />
-        <Icon
-          id="search"
-          className={styles.icon}
-        />
-      </label>
-    </form>
+    <label className={styles.label}>
+      <VisuallyHidden>Search</VisuallyHidden>
+      <input
+        className={styles.input}
+        {...delegated}
+        placeholder="Search"
+      />
+      <Icon
+        id="search"
+        className={styles.icon}
+      />
+    </label>
   );
 }
