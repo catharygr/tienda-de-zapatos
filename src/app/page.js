af07d339278c6b8aa77252e9af711cab37c4211f
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-12 09:06:40 +0000

    Trabajando en page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}></main>
-    </div>
-  );
+  return <main className={styles.main}>Hola</main>;
 }
